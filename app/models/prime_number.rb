762e702e830eaf89dda992163e9c7e744cb077cc
Alok Rawat <alokrawat050@gmail.com>
2017-03-15 02:22:50 +0000

    prime number get and check code

---
+++ app/models/prime_number.rb
@@ -0,0 +1,30 @@
+class PrimeNumber < ActiveRecord::Base
+    def self.get_prime_number_upto(number)
+      start = 2
+      primes = (start..number).to_a
+      (start..number).each do |no|
+        (start..no).each do |num|
+          if ( no % num  == 0) && num != no
+            primes.delete(no)
+            break
+          end
+        end
+      end
+      primes
+    end
+    
+    def self.is_prime_number(n)
+      puts "That's not an integer." unless n.is_a? Integer
+      is_prime = true
+      for i in 2..n-1
+        if n % i == 0
+          is_prime = false
+        end
+      end
+      if is_prime
+        puts "#{n} is prime!"
+      else
+        puts "#{n} is not prime."
+      end
+    end
+end
---
+++ spec/factories/prime_numbers.rb
@@ -0,0 +1,5 @@
+FactoryGirl.define do
+  factory :prime_number do
+    
+  end
+end
